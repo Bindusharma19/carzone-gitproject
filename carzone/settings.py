61954ed7a02346c67579c13300710de3f4b26cf4
Bindusharma19 <binduammadu4@gmail.com>
2022-09-01 21:39:26 +0530

    Completed Teams model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db_1',
+        'USER': 'postgres',
+        'PASSWORD': 'RadhaKrishna12',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
@@ -112,7 +117,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
@@ -124,3 +129,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'carzone/static'),
 ]
 
+#Media settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
